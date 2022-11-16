7c386d4c5ce79738149038f71ffd169eab4f2703
Henrique Guimarães <henrique.guimaraes@ati.com.br>
2022-11-16 13:04:55 -0300

    first commit

---
+++ ati_setup.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+window = Tk()
+window.title("ATI Setup")
+
+label = Label(window, text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+# label.grid(column = 0, row = 0)
+
+
+window.mainloop()
+
+print("hello world")
+
+
